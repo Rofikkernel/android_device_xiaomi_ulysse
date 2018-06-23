159a95f6079493e4fbc6d1be922fc8edf57f82c8
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    ulysse: Build separate system and vendor HIDL libs

--- device.mk
+++ device.mk
@@ -235,7 +235,9 @@ include $(LOCAL_PATH)/rootdir/charger/charger.mk
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # IMS
 PRODUCT_PACKAGES += \
