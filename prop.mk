98af3aa357fa918cfaaae2a0a4e65e6ff94cd0bb
zqm64 <zqm64@outlook.com>
2020-07-20 13:35:36 +0800

    ulysse: Import front flashlight props from ugg
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- prop.mk
+++ prop.mk
@@ -109,6 +109,13 @@ persist.qfp=false
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.fm.transmitter=false
 
+# Front Flashlight
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.s5k3p8sp.flash.low=320 \
+persist.s5k3p8sp.flash.light=300 \
+persist.ov16885.flash.low=290 \
+persist.ov16885.flash.light=275
+
 # Frp
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
