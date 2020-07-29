057fd391f958885f427bb9ae915e25f99dd51256
zqm64 <zqm64@outlook.com>
2020-07-30 02:24:19 +0800

    ulysse: Use Goodix FP HIDL from hardware/xiaomi

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
-    libunwind
+    libunwind \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1
 
 # FM
 PRODUCT_PACKAGES += \
