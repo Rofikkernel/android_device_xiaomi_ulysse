e674b7d5b8cebdd5a99ae507faa7923c6a0d8002
zqm64 <zqm64@outlook.com>
2020-08-07 14:36:40 +0800

    ulysse: vndk: Fix camera and fp dependencies

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -1,3 +1,11 @@
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
     libpowermanager
+
+EXTRA_VENDOR_LIBRARIES := \
+    libbinder \
+    libcutils \
+    libgui \
+    libprocessgroup \
+    libstdc++ \
+    libunwind
