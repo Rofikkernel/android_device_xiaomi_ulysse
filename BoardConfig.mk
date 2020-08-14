zeelog/android_device_xiaomi_mido

mido: Move/Add rild properties to system.prop
* from CAF LA.UM.8.6.r1-04400-89xx.0

Change-Id: I3485c0f8177d4be96bd648fe864d307615b08703

BoardConfig.mk
@@ -40,6 +40,8 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
 # Kernel
 TARGET_KERNEL_CONFIG := mido_defconfig
 BOARD_KERNEL_BASE := 0x80000000

Rofikkernel/android_device_xiaomi_ulysse

BoardConfig.mk
@@ -34,6 +34,8 @@ TARGET_BOARD_PLATFORM := msm8937
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+TARGET_SYSTEM_PROP := $(VENDOR_PATH)/system.prop
+
 # Build
 BUILD_BROKEN_DUP_RULES := true
 